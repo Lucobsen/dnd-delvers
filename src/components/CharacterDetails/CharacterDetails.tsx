a402d183373fe936a7b07535081c7966e1b517dd
Luke Jacobsen <lukejacobsen@gmail.com>
2023-10-20 14:43:04 +0200

    added character levels and prof bonus

--- src/components/CharacterDetails/CharacterDetails.tsx
+++ src/components/CharacterDetails/CharacterDetails.tsx
@@ -1,8 +1,9 @@
-import { Autocomplete, Container, Stack, TextField } from "@mui/material";
+import { Autocomplete, Grid, Stack, TextField } from "@mui/material";
 import React, { useState } from "react";
 import { useRaces } from "../../services/races/races.services";
 import { useClasses } from "../../services/classes/classes.service";
 import { DataItem } from "../../services/api";
+import { getProficiencyBonus, levels } from "../../models/levels.models";
 
 export const CharacterDetails = () => {
   const { races, isFetching: isFetchingRaces } = useRaces();
@@ -10,24 +11,58 @@ export const CharacterDetails = () => {
 
   const [selectedRace, setSelectedRace] = useState<DataItem | null>(null);
   const [selectedClass, setSelectedClass] = useState<DataItem | null>(null);
+  const [selectedLevel, setSelectedLevel] = useState("1");
 
   const [characterName, setCharacterName] = useState("");
   const [characterRace, setCharacterRace] = useState("");
   const [characterClass, setCharacterClass] = useState("");
+  const [characterLevel, setCharacterLevel] = useState("1");
 
   if (isFetchingRaces || isFetchingClasses) return null;
 
   return (
     <>
-      <TextField
-        sx={{ paddingBottom: 2 }}
-        fullWidth
-        value={characterName}
-        label="Name"
-        placeholder="Enter Name"
-        variant="outlined"
-        onChange={(event) => setCharacterName(event.target.value)}
-      />
+      <Grid container spacing={1} pb={2}>
+        <Grid item xs={6}>
+          <TextField
+            fullWidth
+            value={characterName}
+            label="Name"
+            placeholder="Enter Name"
+            variant="outlined"
+            onChange={(event) => setCharacterName(event.target.value)}
+          />
+        </Grid>
+
+        <Grid item xs={3}>
+          <Autocomplete
+            fullWidth
+            value={selectedLevel}
+            onChange={(_, newValue) => newValue && setSelectedLevel(newValue)}
+            inputValue={characterLevel}
+            onInputChange={(_, newValue) => setCharacterLevel(newValue)}
+            disablePortal
+            options={levels}
+            disableClearable
+            renderInput={(params) => (
+              <TextField {...params} label="Level" placeholder="Select Level" />
+            )}
+          />
+        </Grid>
+
+        <Grid item xs={3}>
+          <TextField
+            fullWidth
+            value={getProficiencyBonus(characterLevel)}
+            label="Prof. Bonus"
+            InputProps={{
+              readOnly: true,
+            }}
+            variant="outlined"
+          />
+        </Grid>
+      </Grid>
+
       <Stack
         direction="row"
         alignItems="center"
